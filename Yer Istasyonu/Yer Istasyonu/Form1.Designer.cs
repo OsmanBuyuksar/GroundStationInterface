837c488673f79ddc2a060193afb06b281b4473c9
Osman <56321113+OsmanBuyuksar@users.noreply.github.com>
2021-08-05 15:24:11 +0300

    transfer files to cloud

---
+++ Yer Istasyonu/Yer Istasyonu/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Yer_Istasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RotatingImg = new System.Windows.Forms.PictureBox();
+            this.txtBoxRoll = new System.Windows.Forms.TextBox();
+            this.PixBoxTrial = new System.Windows.Forms.PictureBox();
+            this.txtBoxPitch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.RotatingImg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PixBoxTrial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RotatingImg
+            // 
+            this.RotatingImg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.RotatingImg.Image = ((System.Drawing.Image)(resources.GetObject("RotatingImg.Image")));
+            this.RotatingImg.Location = new System.Drawing.Point(213, 273);
+            this.RotatingImg.Name = "RotatingImg";
+            this.RotatingImg.Size = new System.Drawing.Size(100, 93);
+            this.RotatingImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RotatingImg.TabIndex = 0;
+            this.RotatingImg.TabStop = false;
+            // 
+            // txtBoxRoll
+            // 
+            this.txtBoxRoll.Location = new System.Drawing.Point(213, 372);
+            this.txtBoxRoll.Name = "txtBoxRoll";
+            this.txtBoxRoll.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxRoll.TabIndex = 1;
+            this.txtBoxRoll.TextChanged += new System.EventHandler(this.txtBoxRoll_TextChanged);
+            // 
+            // PixBoxTrial
+            // 
+            this.PixBoxTrial.Image = ((System.Drawing.Image)(resources.GetObject("PixBoxTrial.Image")));
+            this.PixBoxTrial.Location = new System.Drawing.Point(213, 135);
+            this.PixBoxTrial.Name = "PixBoxTrial";
+            this.PixBoxTrial.Size = new System.Drawing.Size(100, 96);
+            this.PixBoxTrial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PixBoxTrial.TabIndex = 2;
+            this.PixBoxTrial.TabStop = false;
+            // 
+            // txtBoxPitch
+            // 
+            this.txtBoxPitch.Location = new System.Drawing.Point(335, 371);
+            this.txtBoxPitch.Name = "txtBoxPitch";
+            this.txtBoxPitch.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxPitch.TabIndex = 3;
+            this.txtBoxPitch.TextChanged += new System.EventHandler(this.txtBoxPitch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 450);
+            this.Controls.Add(this.txtBoxPitch);
+            this.Controls.Add(this.PixBoxTrial);
+            this.Controls.Add(this.txtBoxRoll);
+            this.Controls.Add(this.RotatingImg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.RotatingImg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PixBoxTrial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox RotatingImg;
+        private System.Windows.Forms.TextBox txtBoxRoll;
+        private System.Windows.Forms.PictureBox PixBoxTrial;
+        private System.Windows.Forms.TextBox txtBoxPitch;
+    }
+}
+
---
+++ Yer Istasyonu/Yer Istasyonu/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;  
+using System.Drawing.Imaging;
+
+namespace Yer_Istasyonu
+{
+
+    public partial class Form1 : Form
+    {
+        const float hudCameraViewAngle = 180;
+        Image img = Image.FromFile(@"C:\Users\osman\Desktop\Drone\2021\yer istasyonu\Resimler\arkaplan.png");
+        int roll = 0;
+        int pitch = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void txtBoxRoll_TextChanged(object sender, EventArgs e)
+        {
+            if(int.TryParse(txtBoxRoll.Text,out roll))
+            {
+                 roll = int.Parse(txtBoxRoll.Text);
+                 RotatingImg.BackgroundImage = DrawHUD(RotatingImg, pitch);
+                 RotatingImg.BackgroundImage = Rotate_Image(img,roll,RotatingImg);
+            }
+        }
+        private void txtBoxPitch_TextChanged(object sender, EventArgs e)
+        {
+            if(int.TryParse(txtBoxPitch.Text, out pitch)) 
+            {
+                pitch = Convert.ToInt32(txtBoxPitch.Text);
+                 img = DrawHUD(RotatingImg, pitch);
+                 RotatingImg.BackgroundImage = Rotate_Image(img, roll, RotatingImg);
+            }
+ 
+        }
+        private Image Rotate_Image(Image image, float angle, PictureBox rotatingPicBox)
+        {
+
+            Bitmap img = new Bitmap(image.Width, image.Height);
+
+            Graphics gfx = Graphics.FromImage(img);
+
+            gfx.TranslateTransform(img.Width / 2, img.Height / 2);
+
+            gfx.RotateTransform(angle);
+
+            gfx.DrawImage(image, -img.Width / 2, -img.Height / 2,image.Width,image.Height);
+
+            gfx.Dispose();
+
+            return img;
+
+        }
+        private Image DrawHUD(PictureBox picBox,float angle)
+        {
+            float y = picBox.Size.Height;
+            float g = picBox.Size.Width;
+            Bitmap img = new Bitmap((int)g, (int)y);
+            Graphics gfx = Graphics.FromImage(img);
+
+
+            if(angle < hudCameraViewAngle / 2 && angle > -hudCameraViewAngle/2)
+            {
+                float ratio = (hudCameraViewAngle/2 - angle) / (hudCameraViewAngle/2 + angle);//MathF.Tan(hudCameraViewAngle - angle) / MathF.Tan(hudCameraViewAngle + angle);
+                float sky_h = y / (1 / ratio + 1);
+                float grnd_h = y / (ratio + 1);
+
+
+
+                RectangleF sky = new RectangleF(0,0,g,sky_h);
+                RectangleF grnd = new RectangleF(0, sky_h, g, grnd_h);
+
+                gfx.FillRectangle(Brushes.Aqua, sky);
+                gfx.FillRectangle(Brushes.Brown, grnd);
+            }
+            else if(angle <= hudCameraViewAngle / 2)
+            {
+                RectangleF sky = new RectangleF(0, 0, g, y);
+                gfx.FillRectangle(Brushes.Aqua, sky);
+            }
+            else if(angle >= hudCameraViewAngle / 2)
+            {
+                RectangleF grnd = new RectangleF(0, 0, g, y);
+                gfx.FillRectangle(Brushes.Brown, grnd);
+            }
+            gfx.DrawImage(img, 0, 0, (int)g, (int)y);
+            gfx.Dispose();
+
+            return img;
+        }
+    }
+}
